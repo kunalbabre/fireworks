e6320bb82b85aa1206c6210a5627e7c6851c100e
Kunal Babre <kubabre@microsoft.com>
2019-05-09 12:13:47 +0100

    Add project files.

---
+++ ChatSample/Controller/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ChatSample.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class HomeController : Controller
+{
+    public IHubContext<FireworkHub> _strongFireworkHubContext { get; }
+
+    public HomeController(IHubContext<FireworkHub> fireworkHubContext)
+    {
+        _strongFireworkHubContext = fireworkHubContext;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public bool SingleShot()
+    {
+        return _strongFireworkHubContext.Clients.All.SendAsync("broadcastFirework").IsCompleted;
+    }
+
+
+    public bool MultiShot()
+    {
+        return _strongFireworkHubContext.Clients.All.SendAsync("multiFirework").IsCompleted;
+    }
+}
---
+++ ChatSample/Hubs/FireworkHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSample.Hubs
+{
+    public class FireworkHub : Hub
+    {
+        public void Send()
+        {
+            // Call the FireworksCounter method light a rocket!.
+            Clients.All.SendAsync("broadcastFirework");
+        }
+        public void SendMultiShot()
+        {
+            // Call the FireworksCounter method light a rocket!.
+            Clients.All.SendAsync("multiFirework");
+        }
+    }
+}
